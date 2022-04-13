f24b46d7ae828fa1e8bb312b0b71c7896b152f6a
NayAungLin910 <95837257+NayAungLin910@users.noreply.github.com>
2022-04-13 14:00:10 +0630

    asynchronous usage with axios, and configuring how to mail to gmail inbox

---
+++ resources/views/mail/verification.blade.php
@@ -0,0 +1,2 @@
+<h1>Your Verification Code is {{$data['code']}}</h1>
+<p>This paragraph is written in HTML. And I am just tryin gout mailing from Laravel.</p>
--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,12 @@
 <?php
 
+use App\Mail\VerificationMail;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ItemController;
-use App\Http\Controllers\TestController;
-use App\Http\Middleware\CheckAge;
-use App\Models\User;
 
-Route::get('/test', [TestController::class, 'test']);
-// Route::get('/', [ItemController::class, 'index']);
-Route::get('/', function(){
-    // User::create([
-    //     'first_name'=>'Myo Thant',
-    //     'last_name'=>'Kyaw',
-    //     'email'=>'hello@gmail.com',
-    //     'password'=>'hello', 
-    // ]);
-    // return 'success';
-
-    return User::all();
+Route::get('/sendmail', function(){
+    $data = [
+        'code'=>123456,
+    ];
+    Mail::to('nayaunglin5454@gmail.com')->send(new VerificationMail($data));
 });
-Route::get('/create', [ItemController::class, 'create']);
-Route::post('/create', [ItemController::class, 'store'])->name('item.store');
-
-Route::get('/edit/{id}', [ItemController::class, 'edit'])->name('item.edit');
-Route::put('/edit/{id}', [ItemController::class, 'update'])->name('item.update');
-
-Route::get('/delete/{id}', [ItemController::class, 'delete'])->name('item.delete');
